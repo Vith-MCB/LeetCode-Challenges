5e9abf3486fc73f9cd83af2b843bbedf55648078
Vith-MCB <vitorriblacerda@gmail.com>
2022-12-05 23:34:55 -0300

    problem solved

--- Palindrome Number/Palindrome Number/Program.cs
+++ Palindrome Number/Palindrome Number/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Given an integer x, return true if x is a palindrome, and false otherwise.
+
+using System;
+
+public class Solution
+{
+    public static bool IsPalindrome(int x)
+    {
+        string number = x.ToString();
+        string numberPalindrome = "";
+
+        for (int i = number.Length - 1; i >= 0; i--)
+        {
+            numberPalindrome += number[i];
+        }
+
+        Console.WriteLine(numberPalindrome);
+
+        if (number.Equals(numberPalindrome))
+        {
+            return true;
+        } 
+        else
+        {
+            return false;
+        }
+
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(IsPalindrome(121));
+    }
+}

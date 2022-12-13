e94a30fa965069d5dc31eb01503d559fce109388
Vith-MCB <vitorriblacerda@gmail.com>
2022-12-13 15:33:33 -0300

    Longest Common Prefix

---
+++ Longest Common Prefix/Longest Common Prefix/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public String longestCommonPrefix(String[] strs)
+    {
+        if (strs.Length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.Length; i++)
+            while (strs[i].IndexOf(prefix) != 0)
+            {
+                prefix = prefix.Substring(0, prefix.Length - 1);
+                if (String.IsNullOrEmpty(prefix)) return "";
+            }
+        return prefix;
+    }
+
+}
+

2c372bad71ec78e119e486eebf468f9905d2cea1
Vith-MCB <vitorriblacerda@gmail.com>
2022-12-13 22:46:22 -0300

    Exercism Challenge

---
+++ Cars, Assemble!/Cars, Assemble!/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+static class AssemblyLine
+{
+    public static double SuccessRate(int speed)
+    {
+        if(speed==0) { return 0; }
+        else if(speed >= 1 && speed <= 4) { return 1.0; }
+        else if (speed >= 5 && speed <= 8) { return 0.9; }
+        else if (speed >= 5 && speed <= 8) { return 0.9; }
+        else if (speed == 9) { return 0.8; }
+        else{ return 0.77; }
+    }
+
+    public static double ProductionRatePerHour(int speed)
+    {
+        return (speed * 221) * SuccessRate(speed);
+    }
+
+    public static int WorkingItemsPerMinute(int speed)
+    {
+        return (int)(ProductionRatePerHour(speed)/60);
+    }
+
+
+}
